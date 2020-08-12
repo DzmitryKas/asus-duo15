5e8bcbd7de5559a1f22c7da779062a1ac9c68096
DzmitryKas <Kasperovich_dima@mail.ru>
2020-08-12 16:53:03 +0300

    upd

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,25 @@
+export default function mobileMenu() {
+  const openBtn = document.querySelector('.header__wrapper-menu');
+  const closeBtn = document.querySelector('.header__close');
+  const menu = document.querySelector('.header__nav');
+  
+  openBtn.addEventListener('click', () => {
+    add(menu)
+  })
+
+  closeBtn.addEventListener('click', () => {
+    remove(menu)
+  })
+
+  menu.addEventListener('click', () => {
+    remove(menu)
+  })
+}
+
+function add(element) {
+  element.classList.add('active');
+}
+
+function remove(element) {
+  element.classList.remove('active');
+}
